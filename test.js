b3ddcaf993bc0d8fe1001be588ee456c3418b880
Lunarbaby <lunarbaby21@hotmail.com>
2019-01-30 15:46:39 -0500

    I added comments to my javascript code.

--- test.js
+++ test.js
@@ -68,8 +68,11 @@ function myFunction() {
 
 //Merge Code
 
-//Intron
+// I made an alert to introduce the player to the game.
+
 alert("Welcome to the dungeon. You have been sent down to this dungeon to do one job. Find the treasure hidden deep within but most of the paths will lead to a ‘GAME OVER’. There is however a couple of ways out but you want to leave with the treasure and nothing else will do. You have only one shot so be careful and GOOD LUCK.")
+
+//I used variables to replace repeated strings
 
 let nothing = "You found nothing. Game Over"
 let question = "Which path do you want to take? North, South, East or West"
@@ -77,7 +80,9 @@ let monster = "You found a monster. GAME OVER."
 let findHall = "You found another hall. Please continue."
 let escape = "You found a key to escape. You successfully leave the dungeron but you did not find the treasure. Try again."
 let roomPrompt = "You walk into a very large room. The room is filled with barrels and chest. Barrels are generally filled with food and general supplies so you pass them by for the chests that are everywhere. After walking around the room a few times so you realized that there only a few chest that could contain the treasure.  You only have one shot at picking the correct one, so be careful. Which chest do you want? Pick a number between 0 and 10. Good luck."
+let end = "GAME OVER!"
 
+//This is the code for the game and the first if statment is hall one
 let i = prompt(question);
 
 if (i == "east" ) {
@@ -89,6 +94,8 @@ if (i == "east" ) {
 } else if (i == "west") {
     alert(findHall);
      
+    //This if statement represents hall two
+
       let j = prompt(question);
       if (j == "north") {
          alert(monster)
@@ -96,12 +103,16 @@ if (i == "east" ) {
             alert(nothing)
      } else if (j == "east") {
          alert(findHall);
-         
+        
+         //This if statement represents hall three
+
          let k = prompt(question);
             if (k == "north") {
                 alert(monster);
             } else if (k == "south") {
                 alert("You found the treasure room.")
+                
+                //This if statement represents the treasure room
 
                 let l = prompt(roomPrompt);
 
@@ -114,19 +125,19 @@ if (i == "east" ) {
                         } else if (l == "3") {
                                 alert(escape);
                         } else {
-                                alert("GAME OVER");
+                                alert(end);
                         }
 
             } else if (k == "east" || k == "west"){
                 alert(nothing);
             } else {
-                alert("Game Over");
+                alert(end);
             }
     
         } else {
-         alert('Game Over')
+         alert(end)
      }
 
     } else {
-    alert('Game Over');
+    alert(end);
 }
