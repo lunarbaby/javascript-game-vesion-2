f9ab046b0eccb6a1c9c15ea758ec415a9656ea26
Lunarbaby <lunarbaby21@hotmail.com>
2019-02-04 13:05:19 -0500

    I moved all the code from test.js to main js

--- main.js
+++ main.js
@@ -66,78 +66,195 @@ function myFunction() {
 // }
 
 
-//Merge Code
+// //Merge Code
 
-// I made an alert to introduce the player to the game.
+// // I made an alert to introduce the player to the game.
 
-alert("Welcome to the dungeon. You have been sent down to this dungeon to do one job. Find the treasure hidden deep within but most of the paths will lead to a ‘GAME OVER’. There is however a couple of ways out but you want to leave with the treasure and nothing else will do. You have only one shot so be careful and GOOD LUCK.")
+// alert("Welcome to the dungeon. You have been sent down to this dungeon to do one job. Find the treasure hidden deep within but most of the paths will lead to a ‘GAME OVER’. There is however a couple of ways out but you want to leave with the treasure and nothing else will do. You have only one shot so be careful and GOOD LUCK.")
 
-//I used variables to replace repeated strings
+// //I used variables to replace repeated strings
 
-let nothing = "You found nothing. Game Over"
-let question = "Which path do you want to take? North, South, East or West"
-let monster = "You found a monster. GAME OVER."
-let findHall = "You are now in a hall. Please continue."
-let escape = "You found a key to escape. You successfully leave the dungeron but you did not find the treasure. Try again."
-let roomPrompt = "You walk into a very large room. The room is filled with barrels and chest. Barrels are generally filled with food and general supplies so you pass them by for the chests that are everywhere. After walking around the room a few times so you realized that there only a few chest that could contain the treasure.  You only have one shot at picking the correct one, so be careful. Which chest do you want? Pick a number between 0 and 10. Good luck."
-let end = "GAME OVER!"
+// let nothing = "You found nothing. Game Over"
+// let question = "Which path do you want to take? North, South, East or West"
+// let monster = "You found a monster. GAME OVER."
+// let findHall = "You are now in a hall. Please continue."
+// let escape = "You found a key to escape. You successfully leave the dungeron but you did not find the treasure. Try again."
+// let roomPrompt = "You walk into a very large room. The room is filled with barrels and chest. Barrels are generally filled with food and general supplies so you pass them by for the chests that are everywhere. After walking around the room a few times so you realized that there only a few chest that could contain the treasure.  You only have one shot at picking the correct one, so be careful. Which chest do you want? Pick a number between 0 and 10. Good luck."
+// let end = "GAME OVER!"
 
-//This is the code for the game and the first if statment is hall one
-let i = prompt(question);
+// //This is the code for the game and the first if statment is hall one
+// let i = prompt(question);
 
-if (i == "east" ) {
-    alert(nothing);
-} else if (i =="north"){
-    alert(escape);
-} else if (i == "south") {
-    alert(monster);
-} else if (i == "west") {
-    alert(findHall);
+// if (i == "east" ) {
+//     alert(nothing);
+// } else if (i =="north"){
+//     alert(escape);
+// } else if (i == "south") {
+//     alert(monster);
+// } else if (i == "west") {
+//     alert(findHall);
      
-    //This if statement represents hall two
+//     //This if statement represents hall two
 
-      let j = prompt(question);
-      if (j == "north") {
-         alert(monster)
-     } else if (j == "south" || j == "west") {
-            alert(nothing)
-     } else if (j == "east") {
-         alert(findHall);
+//       let j = prompt(question);
+//       if (j == "north") {
+//          alert(monster)
+//      } else if (j == "south" || j == "west") {
+//             alert(nothing)
+//      } else if (j == "east") {
+//          alert(findHall);
         
-         //This if statement represents hall three
+//          //This if statement represents hall three
 
-         let k = prompt(question);
-            if (k == "north") {
-                alert(monster);
-            } else if (k == "south") {
-                alert("You found the treasure room.")
+//          let k = prompt(question);
+//             if (k == "north") {
+//                 alert(monster);
+//             } else if (k == "south") {
+//                 alert("You found the treasure room.")
                 
-                //This if statement represents the treasure room
+//                 //This if statement represents the treasure room
 
-                let l = prompt(roomPrompt);
+//                 let l = prompt(roomPrompt);
 
-                        if (l == "1" || l == "5" || l =="8") {
-                                alert(monster);
-                        } else if (l == "2" || l == "7" || l == "4") {
-                                alert(nothing);
-                        } else if (l == "9") {
-                                alert("You found the Treasure. YOU WIN!");
-                        } else if (l == "3") {
-                                alert(escape);
-                        } else {
-                                alert(end);
-                        }
+//                         if (l == "1" || l == "5" || l =="8") {
+//                                 alert(monster);
+//                         } else if (l == "2" || l == "7" || l == "4") {
+//                                 alert(nothing);
+//                         } else if (l == "9") {
+//                                 alert("You found the Treasure. YOU WIN!");
+//                         } else if (l == "3") {
+//                                 alert(escape);
+//                         } else {
+//                                 alert(end);
+//                         }
 
-            } else if (k == "east" || k == "west"){
-                alert(nothing);
-            } else {
-                alert(end);
-            }
+//             } else if (k == "east" || k == "west"){
+//                 alert(nothing);
+//             } else {
+//                 alert(end);
+//             }
     
+//         } else {
+//          alert(end)
+//      }
+
+//     } else {
+//     alert(end);
+// }
+
+
+//Game code with functions
+
+//Global variables
+let question = "Which path do you want to take? North, South, East or West"
+let end = "GAME OVER!"
+let findHall = "You are now in a hall. Please continue."
+let nothing = "You found nothing. Game Over"
+let escapeChest = "You found a key to escape. You successfully leave the dungeron but you did not find the treasure. Try again."
+let escapeDoor = "You have found a door. You open the door and find yourself outside. GAME OVER!"
+let trapHall = "You walk down the hall. You run into a dead end so you decided to turn around. As you start turining around spikes come up from the floor and you die instantly. GAME OVER"
+let poisonRoom = "You walk into the room. You notice that there is a chest in the middle of the room. You open the chest and inside is a cloud of posion gas. You died. GAME OVER"
+
+//The function to start the game
+
+function startGame(){
+    entrance();
+}
+
+//The function for the entrance
+
+function entrance (){
+    let welcome = confirm ("Do you want to enter the dungeon?");
+    if (welcome == true){
+        alert(findHall);
+        enterHallOne();
+    }else {
+        alert(end);
+    }
+}
+
+//The function for entering hall one
+
+function enterHallOne (){
+    let i = prompt(question);
+    if (i == "east") {
+        alert(escapeDoor);
+    } else if (i == "north"){
+        entrance();
+    } else if (i == "south"){
+        alert("You have found a room.");
+        alert(poisonRoom);
+    } else if (i == "west") {
+        let j = confirm(findHall);
+        if (j == true) {
+            enterHallTwo();
         } else {
-         alert(end)
-     }
+            entrance();
+        }
+        
+    } else {
+        alert(end);
+    }    
+} 
+
+// The function for hall two
+
+function enterHallTwo () {
+    let i = prompt(question);
+    if (i == "east") {
+        let j = confirm(findHall);
+        if (j == true) {
+            enterHallThree();
+        } else {
+            alert("A cloud of smoke surrounds you and ...");
+            enterHallFour();
+        }
+
+    } else if (i == "north") {
+        alert(escape);
+
+    } else if ( i == "south") {
+        alert(nothing);
+
+    } else if (i == "west") {
+        alert(trapHall);
+    } else {
+        alert(end);
+    }
+}
+
+// The function for hall three
+
+function enterHallThree () {
+    let i = prompt(question);
+    if (i == "east") {
+        alert(poisonRoom);
+    } else if (i == "north") {
+        entrance();
+    } else if (i == "south") {
+        alert(findHall);
+        enterHallFour();
+    } else if (i == "west") {
+        alert(nothing);
 
     } else {
-    alert(end);
+        alert(end);
+    }
 }
+
+// The function for hall four
+
+function enterHallFour () {
+    let i = prompt(question);
+    if (i == "east") {
+        enterHallTwo();
+    } else if (i == "north") {
+        alert (trapHall);
+    } else if (i == "south") {
+        alert(nothing);
+    } else if (i == "west") {
+        alert("You have found the treasure chest. YOU WIN!");
+    }
+}
+
+
