fc634510071ebc5d68b496c0dc666858e9bbcf06
Lunarbaby <lunarbaby21@hotmail.com>
2019-02-04 14:45:21 -0500

    I added code to change the intro paragraph to hall text, removes the button, and adds a text input box

--- test.js
+++ test.js
@@ -1,2 +1,20 @@
 "use strict";
 
+function enterMainHall() {
+
+//This changes the intro paragraph into a new one.
+let para = document.querySelector('p');
+let newPara = document.createElement('p');
+newPara.innerText = "You are now in the main hall. The hall spilts off into four different directions. Please pick a direction: North, South, East, or West.";
+para.parentNode.replaceChild(newPara, para);
+
+//This removes the button
+let deleteButton = document.querySelector('button');
+deleteButton.remove();
+
+//This add text input
+let hallInput = document.createElement('input');
+hallInput.setAttribute('type', 'text');
+newPara.appendChild(hallInput);
+
+}
