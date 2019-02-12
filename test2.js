99d9257018bf9cd998d5a41dc60fa25911b9e87b
Lunarbaby <lunarbaby21@hotmail.com>
2019-02-12 14:28:19 -0500

    I improved the styling of the website a little, worked on the buttons, and decreased repeating code.

--- test2.js
+++ test2.js
@@ -1,5 +1,8 @@
 "use strict";
 
+let buttonDiv = document.querySelector('.button-div');
+
+
 //Add eventlistener to button on the main page
 
 document.getElementById("mainButton").addEventListener("click", enterMainHall);
@@ -8,29 +11,42 @@ document.getElementById("mainButton").addEventListener("click", enterMainHall);
     function reloadPage(){
         location.reload();}
 
+
 //Function that create directions buttons
 function createChoiceButtons(){
-    let newPara = document.querySelector('p');
+    
     //Create North Button
     let northButton = document.createElement('button');
     northButton.innerText = "north";
     northButton.setAttribute("id", "northButton");
-    newPara.appendChild(northButton);
+    buttonDiv.appendChild(northButton);
     //Create South Button
     let southButton = document.createElement('button');
     southButton.innerText = "south";
     southButton.setAttribute("id", "southButton");
-    newPara.appendChild(southButton);
+    buttonDiv.appendChild(southButton);
     //Create East Button
     let eastButton = document.createElement('button');
     eastButton.innerText = "east";
     eastButton.setAttribute("id", "eastButton");
-    newPara.appendChild(eastButton);
+    buttonDiv.appendChild(eastButton);
     //Create West Button
     let westButton = document.createElement('button');
     westButton.setAttribute("id", "westButton");
     westButton.innerText = "west";
-    newPara.appendChild(westButton);
+    buttonDiv.appendChild(westButton);
+    }
+
+    //Function to delete buttons
+    function deleteButtons(){
+    let deleteButton1 = document.querySelector("button");
+        deleteButton1.remove();
+    let deleteButton2 = document.querySelector("button");
+        deleteButton2.remove();
+    let deleteButton3 = document.querySelector("button");
+        deleteButton3.remove();
+    let deleteButton4 = document.querySelector("button");
+        deleteButton4.remove();
     }
 
 //This is the function that represents the main hall
@@ -42,10 +58,9 @@ function enterMainHall() {
     newPara.innerText = "You are now in the main hall. The hall spilts off into four different directions. Please pick a direction: North, South, East, or West.";
     para.parentNode.replaceChild(newPara, para);
 
-
-    //Remove button
-    let deleteButton = document.querySelector('button');
-    deleteButton.remove();
+    //Delete intro button
+    let deleteButtons = document.querySelector('button');
+    deleteButtons.remove();
 
     createChoiceButtons();
    
@@ -61,36 +76,28 @@ function enterMainHall() {
         let text = northButton.innerText;
         if (text == "north"){
             reloadPage();
-        }    else{
-            console.error("function does not work");
-        }
+        } 
     }
     
     function chooseSouthPath(){
         let text = southButton.innerText;
         if (text == "south"){
             poisonRoom();
-        }    else{
-            console.error("function does not work");
-        }
+        }   
     }
 
     function chooseEastPath(){
         let text = eastButton.innerText;
         if (text == "east"){
             escapeRoom();
-        }    else{
-            console.error("function does not work");
-        }
+        }   
     }
   
     function chooseWestPath(){
         let text = westButton.innerText;
         if (text == "west"){
             secondHall();
-        }    else{
-            console.error("function does not work");
-        }
+        }   
     }   
 }
 
@@ -104,17 +111,15 @@ function escapeRoom(){
     newPara.innerText = "You have found a door. You open the door and find yourself outside. GAME OVER!";
     para.parentNode.replaceChild(newPara, para);
 
+    deleteButtons();
+
     //Add new button
 
     let restartButton = document.createElement('button');
     restartButton.innerText = "Restart the game.";
-    newPara.appendChild(restartButton);
+    buttonDiv.appendChild(restartButton);
     restartButton.addEventListener("click", reloadPage);
-
-    
-
-
-}
+    }
 
 //Function that runs if you find a room with a chest with poison in it
 
@@ -126,11 +131,13 @@ function escapeRoom(){
     newPara.innerText = "You walk into the room. You notice that there is a chest in the middle of the room. You open the chest and inside is a cloud of posion gas. You died. GAME OVER";
     para.parentNode.replaceChild(newPara, para);
 
+    deleteButtons();
+
     //Add new button
 
     let restartButton = document.createElement('button');
     restartButton.innerText = "Restart the game.";
-    newPara.appendChild(restartButton);
+    buttonDiv.appendChild(restartButton);
     restartButton.addEventListener("click", reloadPage);
 
 }
@@ -143,6 +150,8 @@ function secondHall(){
     let newPara = document.createElement('p');
     newPara.innerText = "You are now a side hall. The hall spilts off into four different directions. Please pick a direction: North, South, East, or West.";
     para.parentNode.replaceChild(newPara, para);
+
+    deleteButtons();
 
     createChoiceButtons();
 
@@ -163,14 +172,15 @@ function emptyRoom(){
     newPara.innerText = "You walk into a room. You notice that there is a chest in the back of the room. You choose to open the chest and find nothing inside. Try again.";
     para.parentNode.replaceChild(newPara, para);
 
+    deleteButtons();
+
     //Add new button
 
     let restartButton = document.createElement('button');
     restartButton.innerText = "Restart the game.";
     newPara.appendChild(restartButton);
     restartButton.addEventListener("click", reloadPage);
-
-  }
+    }
 
 //The function that runs when a player pick a hall with the treasure room at the end
 
@@ -192,6 +202,8 @@ function trapedHall(){
     newPara.innerText = "You walk down the hall. You run into a dead end so you decided to turn around. As you start turining around spikes come up from the floor and you die instantly. GAME OVER";
     para.parentNode.replaceChild(newPara, para);
 
+    deleteButtons();
+
     //Add new button
 
     let restartButton = document.createElement('button');
@@ -209,6 +221,8 @@ function thirdHall(){
     newPara.innerText = "You are in a very narrow and dark hall. The hall spilts off into four different directions. Please pick a direction: North, South, East, or West.";
     para.parentNode.replaceChild(newPara, para);
 
+    deleteButtons();
+
     createChoiceButtons();
 
     //Add EventListener to direction buttons
@@ -217,21 +231,5 @@ function thirdHall(){
     eastButton.addEventListener("click", trapedHall);
     westButton.addEventListener("click", emptyRoom);
 
-     function poisonRoom(){
-    
-        //This changes the intro paragraph into a new one.
-        let para = document.querySelector('p');
-        let newPara = document.createElement('p');
-        newPara.innerText = "You walk into the room. You notice that there is a chest in the middle of the room. You open the chest and inside is a cloud of posion gas. You died. GAME OVER";
-        para.parentNode.replaceChild(newPara, para);
-    
-        //Add new button
-    
-        let restartButton = document.createElement('button');
-        restartButton.innerText = "Restart the game.";
-        newPara.appendChild(restartButton);
-        restartButton.addEventListener("click", reloadPage);
-    
-         }
     }
 
